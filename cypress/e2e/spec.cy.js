ce212fda6050b878741eb75b4d380e7fc30b63d1
loekehren <loek.ehren@adchieve.com>
2024-04-10 16:03:01 +0200

    Add test case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.clock(Date.UTC(2021, 2, 2));
+    cy.visit('localhost:1338');
+    cy.get('.green').should('have.text', '2021-2-2');
+
+    cy.clock(Date.UTC(2021, 4, 20));
+    cy.reload();
+    cy.get('.green').should('have.text', '2021-4-20');
   })
 })
