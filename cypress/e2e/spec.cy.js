49bdd948442715e2feec466634f45ceb9591d2b6
loekehren <loek.ehren@adchieve.com>
2025-11-24 09:27:08 +0100

    Try with cypress 13.17

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,6 +5,7 @@ describe('page', () => {
     cy.visit('localhost:1338');
     cy.get('.green').should('have.text', '2021-2-2');
 
+    cy.clock().invoke('restore');
     cy.clock(Date.UTC(2021, 4, 20));
     cy.reload();
     cy.get('.green').should('have.text', '2021-4-20');
